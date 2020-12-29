d71b80d8e814cfac521fb9343617260a7a9c39bf
rajesh <rajeshpalle122@gmail.com>
2020-12-29 19:22:09 +0530

    files

---
+++ src/main/kotlin/made/Hello.kt
@@ -0,0 +1,6 @@
+package made
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/made/HelloTest.kt
@@ -0,0 +1,8 @@
+package made
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
